37cce950b5cb4dbf788655c90b7b70277f0c4f98
Your Name <mdrushad0@yahoo.com>
2022-01-26 17:39:55 -0500

    First Commit

---
+++ src/test/java/classAssignments/Java_01.java
@@ -0,0 +1,41 @@
+package classAssignments;
+
+public class Java_01 {
+
+	public static void main(String[] args) {
+
+/*
+		1. Explain data type in Java with example
+		When we use any data in Java, we must announce it to Java what types of data it is so that Java can understand. There are many data types; for example:
+		int		: Any whole number (1,2,3, to infinite)
+		String		: Any texts, numbers, and special characters within double quotes. It starts with a 
+		                               uppercase S; for example:
+		                               ( “meet me @ 23 bridge st.” )
+		char		: It can contain only one character within single quotes; i.e.  ‘a’ , ‘b’ , ‘z’
+		double/ float	: Any numbers with a decimal (.); example: 1.2, 3.5, 8.1 to infinity
+		Boolean	: It represents True or False statement. It can only have true or false states.
+*/
+//		2 what is variable in Java?
+//		A variable is a name assigned to a memory location for Java. It works like a storage space where we can store any data.
+
+//		3 how to store and declare data in Java write the syntax
+//		We can store and declare data in java by announcing the data type, give a name for a variable, and finally providing a value; for example:
+		int Age =27;
+
+//		4. Store data 100 in variable ‘mark’
+		int mark=100;
+
+//		5. Store your school name and print using Java 
+		String school="NextTech";
+		System.out.println(school);
+
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ src/test/java/classAssignments/Java_02_Arithmatics.java
@@ -0,0 +1,28 @@
+package classAssignments;
+
+public class Java_02_Arithmatics {
+
+	public static void main(String[] args) {
+
+		// Assignment:
+		
+		// 1) add 2 whole number and print 
+
+		int number1 = 8;
+		int number2 = 10;
+		int total;
+		total = number1+number2;
+		System.out.println(total);
+		
+		
+		// 2) Subtract 2 whole number and 1 decimal number and print
+		
+		int num_01 = 30;
+		int num_02 = 15;
+		double num_03 = 5.5;
+		double total2 =  num_01-num_02-num_03;
+		System.out.println(total2);		
+
+	}
+
+}
